9b8b1b927b65fd9428c6d38d8168436a4b4f4bd6
serii burduja <seriiburduja@gmail.com>
2025-02-24 17:59:31 +0200

    feat: web router comment

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+// auth and admin
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
