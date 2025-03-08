8def74e5c6fdb3ba86502dc10c1ee0db9341553e
serii burduja <seriiburduja@gmail.com>
2025-03-08 10:11:37 +0200

    feat: user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            AdminSeeder::class
+            AdminSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
