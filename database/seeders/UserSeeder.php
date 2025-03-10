0157757fca0e7d8866b39e04e37810d03a9d08ba
serii burduja <seriiburduja@gmail.com>
2025-03-10 20:43:34 +0200

    feat: seed user with role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,10 +12,21 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $users = [
+            [
             'name' => 'User',
             'email' => 'seriiburduja@gmail.com',
             'password' => bcrypt('12345678'),
-        ]);
+            'role' => 'student',
+            ],
+            [
+            'name' => 'Instructor',
+            'email' => 'instructor@mail.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'instructor',
+            ]
+        ];
+
+        User::insert($users);
     }
 }
