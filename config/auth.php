415da8e85ccc0d6e2af0db961488bc21163bd1a7
serii burduja <seriiburduja@gmail.com>
2025-03-10 15:45:24 +0200

    feat: mail works

--- config/auth.php
+++ config/auth.php
@@ -111,7 +111,7 @@ return [
             'provider' => 'admins',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
-            'throttle' => 60,
+            'throttle' => 5,
         ],
     ],
 
