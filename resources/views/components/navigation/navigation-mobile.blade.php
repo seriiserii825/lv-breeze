28984dd1c7805220fd7052a787f8744fe798df5e
serii burduja <seriiburduja@gmail.com>
2025-03-11 09:45:26 +0200

    feat: student dashboard its working

--- resources/views/components/navigation/navigation-mobile.blade.php
+++ resources/views/components/navigation/navigation-mobile.blade.php
@@ -1,7 +1,7 @@
 <!-- Responsive Navigation Menu -->
 <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
     <div class="pt-2 pb-3 space-y-1">
-        <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        <x-responsive-nav-link :href="route('student.dashboard')" :active="request()->routeIs('student.dashboard')">
             {{ __('Dashboard') }}
         </x-responsive-nav-link>
     </div>
@@ -9,15 +9,11 @@
     <!-- Responsive Settings Options -->
     <div class="pt-4 pb-1 border-t border-gray-200">
         <div class="px-4">
-            <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-            <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+            <div class="text-base font-medium text-gray-800">{{ Auth::user()->name }}</div>
+            <div class="text-sm font-medium text-gray-500">{{ Auth::user()->email }}</div>
         </div>
 
         <div class="mt-3 space-y-1">
-            <x-responsive-nav-link :href="route('profile.edit')">
-                {{ __('Profile') }}
-            </x-responsive-nav-link>
-
             <!-- Authentication -->
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Frontend\UserController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,12 +9,6 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['auth', 'verified', 'check_role:student'], 'prefix' => 'student', 'as' => 'student.'], function () {
     Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::get('/admin/dashboard', function () {
