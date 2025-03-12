36aff390def83c1b21850ff609419513fba1c896
serii burduja <seriiburduja@gmail.com>
2025-03-12 20:24:28 +0200

    feat: forgot password with new components

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -9,12 +9,7 @@
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        <x-email-component name="email" label="Email" placeholder="Your email" required="true" />
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
