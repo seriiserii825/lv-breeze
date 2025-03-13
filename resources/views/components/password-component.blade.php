504b41fa412452140637a1acfc72380570acb8d9
serii burduja <seriiburduja@gmail.com>
2025-03-13 07:56:55 +0200

    feat: toggle password

--- resources/views/components/password-component.blade.php
+++ resources/views/components/password-component.blade.php
@@ -4,9 +4,10 @@
         <label class="form-label">{{ $label }}</label>
     @endif
     <div class="input-group input-group-flat">
-        <input type="password" name="{{ $name }}" {{ $required ? 'required' : null }} class="form-control" placeholder="{{ $placeholder }}" autocomplete="off">
+        <input type="password" name="{{ $name }}" {{ $required ? 'required' : null }} class="form-control"
+            placeholder="{{ $placeholder }}" autocomplete="off">
         <span class="input-group-text">
-            <a href="#" class="link-secondary" title="Show password"
+            <a href="#" class="link-secondary js-toggle-password" title="Show password"
                 data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
                     stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
@@ -20,3 +21,16 @@
     </div>
 </div>
 <x-input-error :messages="$errors->get($name)" class="mt-2" />
+<script charset="utf-8">
+    document.querySelectorAll('.js-toggle-password').forEach(function(toggle) {
+        toggle.addEventListener('click', function(e) {
+            e.preventDefault();
+            var input = e.currentTarget.closest('.input-group').querySelector('input');
+            if (input.getAttribute('type') == 'password') {
+                input.setAttribute('type', 'text');
+            } else {
+                input.setAttribute('type', 'password');
+            }
+        });
+    });
+</script>
