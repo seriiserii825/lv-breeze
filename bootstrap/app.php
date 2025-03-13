404e95b2751bb37ddd0896621930b2be8c9b19f1
serii burduja <seriiburduja@gmail.com>
2025-03-13 07:48:53 +0200

    feat: app exceptions for admin dashboard to login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,8 @@
 <?php
-
+use Illuminate\Auth\AuthenticationException;
 use App\Http\Middleware\CheckRoleMiddleware;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -9,8 +11,8 @@ use App\Http\Middleware\RedirectIfAuthenticated;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -21,5 +23,15 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Handle unauthenticated exceptions
+        $exceptions->render(function (AuthenticationException $e, Request $request): Response {
+            $guard = $e->guards()[0] ?? null;
+
+            $loginRoute = match ($guard) {
+                'admin' => route('admin.login'),
+                default => route('login'),
+            };
+
+            return redirect()->guest($loginRoute);
+        });       //
     })->create();
