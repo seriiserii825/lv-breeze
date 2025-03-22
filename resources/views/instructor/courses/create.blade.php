91811216b75f7b21f4a55408ce8c2262bf6fbc27
serii burduja <seriiburduja@gmail.com>
2025-03-22 07:45:21 +0200

    feat: refactor course create

--- resources/views/instructor/courses/create.blade.php
+++ resources/views/instructor/courses/create.blade.php
@@ -1,55 +1,33 @@
 <x-layouts.course-create-layout>
     <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
         <div class="add_course_basic_info">
-            <form action="{{ route('instructor.courses.store') }}" method="post" id="js-create-course-step-1" enctype="multipart/form-data">
+            <form action="{{ route('instructor.courses.store') }}" method="post" id="js-create-course-step-1"
+                enctype="multipart/form-data">
                 @csrf
                 <div class="row">
-                    <div class="col-xl-12">
-                        <div class="add_course_basic_info_imput">
-                            <x-form.input label="Title" name="title" placeholder="Title" />
-                        </div>
+                    <div class="col-xl-12 add_course_basic_info_imput">
+                        <x-form.input label="Title" name="title" placeholder="Title" />
                     </div>
-                    <div class="col-xl-12">
-                        <div class="add_course_basic_info_imput">
-                            <x-form.input label="Seo description" name="seo_description"
-                                placeholder="Seo description" />
-                        </div>
+                    <div class="col-xl-12 add_course_basic_info_imput">
+                        <x-form.input label="Seo description" name="seo_description" placeholder="Seo description" />
                     </div>
-                    <div class="col-xl-12">
-                        <div class="add_course_basic_info_imput">
-                            <x-form.input-file label="Thumbnail" name="thumbnail" />
-                        </div>
+                    <div class="col-xl-12 add_course_basic_info_imput">
+                        <x-form.input-file label="Thumbnail" name="thumbnail" />
                     </div>
-                    <div class="col-xl-6">
-                        <div class="add_course_basic_info_imput">
-                            <x-form.select label="Demo Video Storage" name="demo_video_storage" :options="$enum_values" />
-                            <!-- <label for="#">Demo Video Storage <b>(optional)</b></label> -->
-                            <!-- <select class="select_js" name="demo_video_storage"> -->
-                            <!--     <option value=""> Please Select </option> -->
-                            <!--     <option value="">Red</option> -->
-                            <!--     <option value="">Black</option> -->
-                            <!--     <option value="">Orange</option> -->
-                            <!--     <option value="">Rose Gold</option> -->
-                            <!--     <option value="">Pink</option> -->
-                            <!-- </select> -->
-                        </div>
+                    <div class="col-xl-6 add_course_basic_info_imput">
+                        <x-form.select label="Demo Video Storage" name="demo_video_storage" :options="$enum_values" />
                     </div>
-                    <div class="col-xl-6">
-                        <div class="add_course_basic_info_imput">
-                            <x-form.input label="Price" name="price" placeholder="Price" />
-                            <p>Put 0 for free</p>
-                        </div>
+                    <div class="col-xl-6 add_course_basic_info_imput">
+                        <x-form.input label="Price" name="price" placeholder="Price" />
+                        <p>Put 0 for free</p>
                     </div>
-                    <div class="col-xl-6">
-                        <div class="add_course_basic_info_imput">
-                            <x-form.input label="Discount Price" name="discount" type="number" placeholder="Discount Price" />
-                        </div>
+                    <div class="col-xl-6 add_course_basic_info_imput">
+                        <x-form.input label="Discount Price" name="discount" type="number"
+                            placeholder="Discount Price" />
                     </div>
-                    <div class="col-xl-12">
-                        <div class="mb-0 add_course_basic_info_imput">
-                            <x-form.textarea label="Description" name="description" placeholder="Description" />
-                            <button type="submit" class="common_btn mt_20">Save</button>
-                        </div>
+                    <div class="col-xl-12 mb-0 add_course_basic_info_imput">
+                        <x-form.textarea label="Description" name="description" placeholder="Description" />
+                        <button type="submit" class="common_btn mt_20">Save</button>
                     </div>
                 </div>
             </form>
