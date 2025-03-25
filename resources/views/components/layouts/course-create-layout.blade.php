93a3d31ed90a6710d3684c0de67c633f50422c07
serii burduja <seriiburduja@gmail.com>
2025-03-25 13:22:49 +0200

    feat: edit step 3 refactor

--- resources/views/components/layouts/course-create-layout.blade.php
+++ resources/views/components/layouts/course-create-layout.blade.php
@@ -17,26 +17,3 @@
         </div>
     </div>
 </x-layouts.front-user-layout>
-<script charset="utf-8">
-    const select = document.querySelector('select[name="demo_video_storage"]');
-    if (select) {
-        select.addEventListener('change', changeSelect);
-        changeSelect();
-    }
-
-    function changeSelect() {
-        const value = select.value;
-        const videoFile = document.querySelector('#js-video-file');
-        const videoInput = document.querySelector('#js-video-input');
-        if (value === 'upload') {
-            videoFile.classList.remove('d-none');
-            videoInput.classList.add('d-none');
-            videoInput.querySelector('input').value = '';
-        } else {
-            videoFile.classList.add('d-none');
-            videoInput.classList.remove('d-none');
-            videoFile.querySelector('input').value = '';
-        }
-    }
-</script>
-
